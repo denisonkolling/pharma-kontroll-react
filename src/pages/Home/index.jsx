ddf0e37558fad6bbe40701c61b36a5baaab6f7a8
Denison Kolling <denisonkolling@gmail.com>
2023-08-14 09:18:46 -0300

    feat: cria tela home

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Title } from './styles';
+
+const Home = () => {
+	return (
+		<>
+			<Title>Home Page</Title>
+			<a href="#">Login</a>
+		</>
+	);
+};
+
+export default Home;
