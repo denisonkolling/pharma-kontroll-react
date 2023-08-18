e2feaf63cd4fb22e0f05b9ee0da208fc65148360
Denison Kolling <denisonkolling@gmail.com>
2023-08-18 07:48:25 -0300

    feat: adiciona auth context e hook use local storage

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+const useLocalStorage = (key, initialValue) => {
+	const storedValue = localStorage.getItem(key);
+
+	const initial = storedValue ? JSON.parse(storedValue) : initialValue;
+
+	const [value, setValue] = useState(initial);
+
+	const updateValue = (newValue) => {
+		localStorage.setItem(key, JSON.stringify(newValue));
+    setValue(newValue)
+	};
+
+	return [value, setValue];
+}
+
+export default useLocalStorage;
