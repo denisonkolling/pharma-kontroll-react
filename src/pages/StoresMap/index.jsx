4c3480aefca704b3ff292580a6e96e4678060091
Denison Kolling <denisonkolling@gmail.com>
2023-08-20 20:41:02 -0300

    fix: importacao paginas renomeia arquivos paginas novamente

--- src/pages/StoresMap/index.jsx
+++ src/pages/StoresMap/index.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useContext } from 'react';
-import { Container, Content, Title, Wrapper } from './StoreMapsStyles';
+import { Container, Content, Title, Wrapper } from './styles';
 import Map from '../../components/Map';
 import { StoreContext } from '../../context/StoreContext';
 import Sidebar from '../../components/Sidebar';
