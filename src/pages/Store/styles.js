febffcfe1b5ab21a84a5e88523d27069f65c2e0b
Denison Kolling <denisonkolling@gmail.com>
2023-08-22 09:44:59 -0300

    style: altera estilo responsividade formulario cadastro farmacia

--- src/pages/Store/styles.js
+++ src/pages/Store/styles.js
@@ -26,40 +26,46 @@ export const Content = styled.div`
 	padding: 20px;
 `;
 
-export const Label = styled.label`
-	line-height: 18px;
-	font-size: 18px;
-	font-weight: 400;
-	color: black;
-	width: 50%;
-	padding: 15px;
-`;
-
 export const Form = styled.form`
-	gap: 10px;
-	width: 100%;
-`;
-
-export const Row = styled.div`
-	display: flex;
-	margin-top: 10px;
-	@media (max-width: 768px) {
-	flex-direction: column;
-	}
+	padding: 10px;
 `;
 
 export const Buttons = styled.div`
 	display: flex;
-	margin-top: 25px;
+	margin: 25px 15px;
 	justify-content: end;
+	@media (max-width: 770px) {
+		justify-content: center;
+	}
 `;
 
+export const Label = styled.label`
+	line-height: 28px;
+	font-size: 18px;
+	font-weight: 400;
+	color: black;
+	width: 100%;
+`;
 export const Input = styled.input`
-	padding: 16px 20px;
+	padding: 10px;
 	width: 100%;
 	border-radius: 5px;
 	font-size: 16px;
-
 	background-color: #f0f2f5;
 	border: 1px solid darkgrey;
+`;
+export const Row = styled.div`
+	display: flex;
+	margin-top: 10px;
+	align-items: center;
+	justify-content: space-between;
+	@media (max-width: 768px) {
+		flex-direction: column;
+	}
+`;
+export const Column = styled.div`
+display: flex;
+flex-direction: column;
+width: 100%;
+padding: 0px 15px; 
 `;
